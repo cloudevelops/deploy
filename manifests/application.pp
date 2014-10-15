d5f6945fa82ae2ac40841091906a33c2719f0e35
Zdenek Janda <zdenek.janda@cloudevelops.com>
2014-10-15 16:25:43 +0200

    added role

--- manifests/application.pp
+++ manifests/application.pp
@@ -8,8 +8,6 @@ define deploy::application (
 
   $application_role = $original_name
 
-  notify{"application start}":}
-
   if ($role == $application_role) {
     notify{"application role ok: ${application_role}":}
   }

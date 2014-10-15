030a046c5980280e52d653bd29e8ebf12e2ac1f3
Zdenek Janda <zdenek.janda@cloudevelops.com>
2014-10-15 16:12:33 +0200

    added role

--- manifests/application.pp
+++ manifests/application.pp
@@ -2,15 +2,15 @@ define deploy::application (
   $application,
   $hiera_scope,
   $role,
-  $application_role = $original_name,
+  $original_name,
 ) {
+
+  $application_role = $original_name
 
   notify{"application start}":}
 
   if ($role == $application_role) {
-
     notify{"application role ok: ${application_role}":}
-
   }
 
 }
